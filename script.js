6292683d30919d77e5d28fa895f7926a9248eb5d
PowerSliva <71924695+PowerSliva@users.noreply.github.com>
2020-12-24 16:05:49 +0700

    res

--- script.js
+++ script.js
@@ -1,58 +1,15 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    
-    type: 'line',
-  
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'График линейной функции: y = 2x - 25',
-            borderColor: 'rgb(25,25, 254)',
-            data: [],
-            backgroundColor: 'transparent',
-          cubicInterpolationMode: ''
-        }]
-    },
-
-    options: {}
-});
-
-//Создаем точки для нашего графика
-
-let i = 0;
-function addChart(){
-  
-   let x = Number(document.getElementById('x').value);
-   let newx = x;
-   let y = 2*x - 25;        
-  
-//Условие на следующее число (должно быть больше предыдущего)
-  
-      if(i === 0){
-       chart.data.labels.push(x);
-       chart.data.datasets[0].data.push(y);
-       chart.update();
-      }
-         if(i > 0){
-         chart.data.labels.push(x);
-         chart.data.datasets[0].data.push(y);
-         chart.update();
-         } 
-  
-            if(chart.data.labels[i-1] >= newx && i > 0){
-            chart.data.labels.pop(x);
-            chart.data.datasets[0].data.pop(y);
-            chart.update();
-            i--;
-            }
-i++;
+function func(){
+    (async () => {
+      let url = 'https://jsonplaceholder.typicode.com/posts'; 
+        let response = await fetch(url);
+          let posts = await response.json();
+let i = 0
+while(i<10){
+  let div = document.createElement('div');
+      div.className = "spisok";
+      div.innerHTML =(`<strong>Страница: </strong>${posts[i].title}` +`<br/><strong>id страницы: </strong>${posts[i].id}`+ `<br/><strong>Тело страницы: </strong>${posts[i].body}`);
+      one.append(div);
+i++
 }
-
-//Очистка графика
-
-function clearChart(){
-      chart.data.labels.length = 0;
-      chart.data.datasets[0].data.length = 0
-      chart.update();
-      console.log(chart.data.labels);
+})()
 }
